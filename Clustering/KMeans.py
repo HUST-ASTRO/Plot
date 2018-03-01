b567ed257d625d2dbdb4ea597e94ba5cbc1d62b9
yuliumt <yuliumutian@gmail.com>
2018-03-01 15:02:19 +0800

    Add KMeans code

--- Clustering/KMeans.py
+++ Clustering/KMeans.py
@@ -6,6 +6,7 @@
 # @Software: PyCharm
 
 
+import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 from sklearn import metrics
@@ -15,6 +16,12 @@ from sklearn.preprocessing import StandardScaler
 
 class InputData:
     def __init__(self, data, scaled=False):
+        self._method_choices = {'scatter': self._scatter,
+                                'color': self._color}
+        self.method = None
+        self.label = None
+        self.centroids = None
+        self.km = None
 
         # Scaled data(standard normal)
         if scaled:
@@ -35,36 +42,77 @@ class InputData:
         plt.xticks(n)
         plt.plot(n, score, 'o-')
         plt.show()
-# class KMeansVisual:
-#     def __init__(self, data, n_clusters, method, scaled=False):
-#
-#         # Scaled data
-#         if scaled == True:
-#             scaler = StandardScaler()
-#             data = scaler.fit_transform(data)
-#
-#         km = KMeans(n_clusters=n_clusters).fit(data)
-#
-#         self._method_choices = {'scatter': self._scatter,
-#                                 'color': self._color}
-#
-#         # simple test to validate method
-#         if method in self._method_choices.keys():
-#             self.method = method
-#         else:
-#             raise ValueError("Invalid Value for method: {0}".format(method))
-#
-#         self._method_choices[self.method]()
-#
-#     def _scatter(self):
-#         print()
-#
-#     def _color(self):
-#         print()
+
+        return score
+
+    def clustering(self, n_clusters):
+        km = KMeans(n_clusters=n_clusters).fit(self.data)
+
+        self.km = km
+        self.label = km.labels_
+        self.centroids = km.cluster_centers_
+
+    def plot(self, method):
+        # simple test to validate method
+        if method in self._method_choices.keys():
+            self.method = method
+        else:
+            raise ValueError("Invalid Value for method: {0}".format(method))
+
+        self._method_choices[self.method]()
+
+    def _scatter(self):
+        numSamples = len(self.data)
+        labels = self.label
+        mark = ['r.', 'b.']
+
+        # 画出所有样例点 属于同一分类的绘制同样的颜色
+        for i in range(numSamples):
+            # markIndex = int(clusterAssment[i, 0])
+            plt.plot(self.data[i][0], self.data[i][1], mark[labels[i]])  # mark[markIndex])
+        centroids = self.centroids
+        for i in range(2):
+            plt.plot(centroids[i][0], centroids[i][1], 'k+', markersize=18)
+            # print centroids[i, 0], centroids[i, 1]
+        plt.title('offset-T90', size=14)
+        plt.xlabel('T90', size=14)
+        plt.ylabel('offset', size=14)
+
+    def _color(self):
+        # Step size of the mesh. Decrease to increase the quality of the VQ.
+        h = .01  # point in the mesh [x_min, x_max]x[y_min, y_max].
+
+        # Plot the decision boundary. For that, we will assign a color to each
+        x_min, x_max = self.data['T90'].min() - 0.2, self.data['T90'].max() + 0.2
+        y_min, y_max = self.data['offset'].min() - 0.2, self.data['offset'].max() + 0.2
+        xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+
+        # Obtain labels for each point in mesh. Use last trained model.
+        Z = self.km.predict(np.c_[xx.ravel(), yy.ravel()])
+
+        # Put the result into a color plot
+        Z = Z.reshape(xx.shape)
+
+        plt.imshow(Z, interpolation='nearest',
+                   extent=(xx.min(), xx.max(), yy.min(), yy.max()),
+                   cmap=plt.get_cmap('bwr'),
+                   aspect='auto', origin='lower')
+
+        self.data['scaled_cluster'] = self.label
+        centers = self.data.groupby('scaled_cluster').mean().reset_index()
+
+        # score = metrics.silhouette_score(data, Table.scaled_cluster)
+        # print(score)
+
+        # pd.scatter_matrix(Table, c=colours[Table.scaled_cluster], alpha=1, figsize=(10, 10), s=100)
+        plt.scatter(self.data['T90'], self.data['offset'], c='k')
+        plt.scatter(centers.T90, centers.offset, linewidths=3, marker='+', s=300, c='w')
+        # plt.title('offset-T90')
+        plt.xlabel('T90')
+        plt.ylabel('offset')
 
 
 if __name__ == '__main__':
     filename = u'/home/hust/Desktop/Myproject/astroML/KMeans/data/Table_new.csv'
     Table = pd.read_csv(filename, usecols=['offset', 'T90']).dropna()
     a = InputData(Table)
-    a.silhouette()
